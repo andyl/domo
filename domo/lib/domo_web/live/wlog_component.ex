26d5b49c4ade3fd4353268350b40a0a593e6df8b
andyl <andy@r210.com>
2023-03-13 17:06:54 -0700

    Start to setup wlog page

---
+++ domo/lib/domo_web/live/wlog_component.ex
@@ -0,0 +1,46 @@
+defmodule DomoWeb.WlogComponent do
+  use Phoenix.Component
+
+  attr :secs, :integer
+  attr :text, :string
+
+  def start_link(assigns) do
+    ~H"""
+    <div style="text-align: center;">
+      <a href="#" phx-click="start-period" phx-value-secs={@secs}>
+        <%= @text %>
+      </a>
+      <br/>
+      <small><%= @label %></small>
+    </div>
+    """
+  end
+
+  # def start_link(%{mins: a_mins} = assigns) do
+  #   mins = a_mins |> String.to_integer()
+  #   secs = mins * 60
+  #   ~H"""
+  #   <div style="text-align: center;">
+  #     <a href="#" phx-click="start-period" phx-value-secs={secs}>
+  #       <%= mins %> mins
+  #     </a>
+  #     <br/>
+  #     <small><%= @label %></small>
+  #   </div>
+  #   """
+  # end
+  #
+  # def start_link(%{secs: a_secs} = assigns) do
+  #   secs = a_secs |> String.to_integer()
+  #   ~H"""
+  #   <div style="text-align: center;">
+  #     <a href="#" phx-click="start-period" phx-value-secs={secs}>
+  #       <%= secs %> secs
+  #     </a>
+  #     <br/>
+  #     <small><%= @label %></small>
+  #   </div>
+  #   """
+  # end
+
+end
---
+++ domo/priv/static/img/README.md
@@ -0,0 +1,6 @@
+# Favicons 
+
+Source: 
+
+https://www.iconsdb.com/lime-icons/time-5-icon.html
+
